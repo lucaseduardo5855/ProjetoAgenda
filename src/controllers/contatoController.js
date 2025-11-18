3ac1b490969d97b1e8733ed5951db476ca1d8764
Lucas Eduardo de Melo <136135204+lucaseduardo5855@users.noreply.github.com>
2025-11-17 23:13:30 -0300

    editando contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -52,4 +52,25 @@ exports.editIndex = async function (req, res) {
   }
 };
 
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) return res.render('404');
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
 
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('/contato/index'));
+      return;
+    }
+
+    // Bloco de SUCESSO
+    req.flash('success', 'Contato editado com sucesso!');
+    // Redireciona para a página de EDIÇÃO do contato que acabou de ser criado
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+  } catch (e) {
+    console.log(e);
+    res.render('404');
+  }
+};
